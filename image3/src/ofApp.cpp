67c7d3e13427b2b770d5700400762c00fb579282
opheliaming <opheliaming@gmail.com>
2020-06-24 01:44:38 +0200

    update Dithering 2

--- image3/src/ofApp.cpp
+++ image3/src/ofApp.cpp
@@ -3,32 +3,33 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     img.load("ML.png");
-    int factor = 1;
-    mona.allocate(img.getWidth(),img.getHeight(),OF_IMAGE_COLOR);
-    for(int i = 0; i < img.getWidth();i++){
-      for(int j = 0; j < img.getHeight();j++){
-        int i_m_1 = ofClamp(i-1, 0, img.getWidth()-1);
-        int i_p_1 = ofClamp(i+1, 0, img.getWidth()-1);
-        int j_m_1 = ofClamp(j-1, 0, img.getHeight()-1);
-        int j_p_1 = ofClamp(j+1, 0, img.getHeight()-1);
-        int m = int(img.getColor(i,j).getBrightness());
-        int n_ = int(img.getColor(i,j_p_1).getBrightness());
-        int ne = int(img.getColor(i_p_1,j_p_1).getBrightness());
-        int e_ = int(img.getColor(i_p_1,j).getBrightness());
-        int se = int(img.getColor(i_p_1,j_m_1).getBrightness());
-        int s_ = int(img.getColor(i,j_m_1).getBrightness());
-        int sw = int(img.getColor(i_m_1,j_m_1).getBrightness());
-        int w_ = int(img.getColor(i_m_1,j).getBrightness());
-        int nw = int(img.getColor(i_m_1,j_p_1).getBrightness());
-        int oldpixel = int(img.getColor(i,j).getBrightness());
-        int newpixel = (oldpixel*factor/255) *(255/factor);   //find_closest_palette_color
+    int factor = 5;
+    mona.setFromPixels(img.getPixels());
+    //mona.allocate(img.getWidth(),img.getHeight(),OF_IMAGE_COLOR);
+    for(int i = 0; i < mona.getWidth();i++){
+      for(int j = 0; j < mona.getHeight();j++){
+        int i_m_1 = ofClamp(i-1, 0, mona.getWidth()-1);
+        int i_p_1 = ofClamp(i+1, 0, mona.getWidth()-1);
+        int j_m_1 = ofClamp(j-1, 0, mona.getHeight()-1);
+        int j_p_1 = ofClamp(j+1, 0, mona.getHeight()-1);
+        int m = mona.getColor(i,j).getBrightness();
+        int n_ = mona.getColor(i,j_p_1).getBrightness();
+        int ne = mona.getColor(i_p_1,j_p_1).getBrightness();
+        int e_ = mona.getColor(i_p_1,j).getBrightness();
+        int se = mona.getColor(i_p_1,j_m_1).getBrightness();
+        int s_ = mona.getColor(i,j_m_1).getBrightness();
+        int sw = mona.getColor(i_m_1,j_m_1).getBrightness();
+        int w_ = mona.getColor(i_m_1,j).getBrightness();
+        int nw = mona.getColor(i_m_1,j_p_1).getBrightness();
+        int oldpixel = m;
+        int newpixel = int((oldpixel*factor/255) *(255/factor)); //find_closest_palette_color
         int err = oldpixel - newpixel;
           
         mona.setColor(i,j, newpixel);
-        mona.setColor(i_p_1,j, e_ + err * 7 / 16 );
-        mona.setColor(i_m_1,j_p_1, nw + err * 3 / 16);
-        mona.setColor(i,j_p_1, n_+err * 5 / 16);
-        mona.setColor(i_p_1,j_p_1, ne+ err * 1 / 16);
+        mona.setColor(i_p_1,j, e_ + err * 7.0 / 16.0 );
+        mona.setColor(i_m_1,j_p_1, nw + err * 3.0 / 16.0);
+        mona.setColor(i,j_p_1, n_+err * 5.0 / 16.0);
+        mona.setColor(i_p_1,j_p_1, ne+ err * 1.0 / 16.0);
       }
     }
     mona.update();
